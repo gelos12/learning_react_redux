e527185c4fbd26439be5701857c776956bebb407
unknown <gelos2012@gmail.com>
2019-06-30 00:07:58 +0900

    redux actions contructor

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from "redux";
 
-export default class BookList extends Component{
+class BookList extends Component{
     renderList(){
         return this.props.books.map(book=>{
             return (
-                <li key="book.title"
+                <li key={book.title}
+                    onClick={() => this.props.selectBook(book)}
                     className="list-group-item"
                 >{book.title}</li>
             );
@@ -18,4 +22,16 @@ export default class BookList extends Component{
             </ul>
         )
     }
-};
+}
+
+function mapStateToProps(state){
+    return {
+        books : state.books
+    };
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({selectBook : selectBook},dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default () => {
     return [
-        {title: '해리포터'},
+        {title: '해리포터:마법사의 돌'},
         {title: '검은 문'},
         {title: '어벤져스'},
         {title: '존윅'},
         {title: '애나벨'},
         {title: '리액트완벽정복'}
-    ]
+    ];
 };
